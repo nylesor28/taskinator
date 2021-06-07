74ef8a8c528c7cf0dfb56d3afe5143c476c6010a
RD <81473475+nylesor28@users.noreply.github.com>
2021-06-07 16:18:45 -0400

    add event listening to the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -10,7 +12,7 @@ var createTaskHandler = function() {
   }
 
 
-  buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
   
